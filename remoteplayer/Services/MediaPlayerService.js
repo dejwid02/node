1b2bef40d1842442484a0c6f169eb4d009c28878
dejwid02 <dejwid02@wp.pl>
2019-01-17 18:40:36 +0100

    fix eslint issues

--- remoteplayer/Services/MediaPlayerService.js
+++ remoteplayer/Services/MediaPlayerService.js
@@ -1,23 +1,22 @@
-
 const playerPath = 'C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe';
-var playerProcess;
+let playerProcess;
 let playing = false;
-const {spawn} = require('child_process');
+const { spawn } = require('child_process');
+
+function stop() {
+  if (playing && typeof playerProcess !== 'undefined') {
+    playerProcess.kill();
+    playerProcess = undefined;
+  }
+}
 
 function play(mediaPath) {
-    if(playing) {
-        stop();
-    }
-    playerProcess = spawn(playerPath, [mediaPath]);
-    playing=true;
-}
-function stop() {
-    if (playing && typeof playerProcess!=='undefined')
-    {
-       playerProcess.kill();
-       playerProcess=undefined; 
-    }
+  if (playing) {
+    stop();
+  }
+  playerProcess = spawn(playerPath, [mediaPath]);
+  playing = true;
 }
 
 module.exports.stop = stop;
-module.exports.play = play; 
+module.exports.play = play;
--- remoteplayer/routes/index.js
+++ remoteplayer/routes/index.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var router = express.Router();
-const player = require('../Services/MediaPlayerService')
+const express = require('express');
+
+const router = express.Router();
+const player = require('../Services/MediaPlayerService');
+
 const streamUrls = [
-  ['http://stream3.polskieradio.pl:8900','PR 1'],
+  ['http://stream3.polskieradio.pl:8900', 'PR 1'],
   ['http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p', 'BBC Radio 1'],
   ['http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio2_mf_p', 'BBC Radio 2'],
   ['http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio3_mf_p', 'BBC Radio 3'],
@@ -10,23 +12,22 @@ const streamUrls = [
   ['http://stream.resonance.fm:8000/resonance', 'ResonanceFM'],
   ['http://185.85.29.166:8000', 'OE 3'],
   ['http://addrad.io/4WRNvc', 'WDR 2'],
-  ['http://stream85.polskieradio.pl/omniaaxe/k16.stream/playlist.m3u8', 'Koncerty w 3']
-  
-];
+  ['http://stream85.polskieradio.pl/omniaaxe/k16.stream/playlist.m3u8', 'Koncerty w 3']];
 
-var process;
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Express',
+    stations: streamUrls,
+  });
 });
 
-router.get('/play/:id', function(req, res, next) {
-  player.play(streamUrls[req.params.id][0])
+router.get('/play/:id', (req, res) => {
+  player.play(streamUrls[req.params.id][0]);
   res.render('index', { title: `Im playing ${streamUrls[req.params.id][1]}` });
 });
 
-router.get('/stop', function(req, res, next) {
-  
+router.get('/stop', (req, res) => {
   player.stop();
   res.render('index', { title: 'Im playing' });
 });
