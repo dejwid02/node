2714a4cf899516d3404b344e918fc9fe6428a4d6
dejwid02 <dejwid02@wp.pl>
2019-02-03 16:49:33 +0100

    play from component ts file

--- remoteplayer/app_public/src/app/stationslist/stationslist.component.ts
+++ remoteplayer/app_public/src/app/stationslist/stationslist.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 export class Station {
   id: Number;
-  name: String;
-  url: String;
-  imageUrl: String
+  name: string;
+  url: string;
+  imageUrl: string
 }
 @Component({
   selector: 'app-stationslist',
@@ -18,15 +18,18 @@ export class StationslistComponent implements OnInit {
   stations: Station[] = [
     {id: 1,
     imageUrl:'http://www.portalmedialny.pl/media/images/original/md5/e/c/ec4de4217b056cb7a3fb73dd7ce94135/polskie_radio_30.jpg' ,
-    url: encodeURIComponent('http://stream3.polskieradio.pl:8900'),
+    url: 'http://stream3.polskieradio.pl:8900',
     name: 'PR1'},
     {id: 2,
       imageUrl:'https://ichef.bbci.co.uk/images/ic/640x360/p02lrp2x.jpg' ,
-      url: encodeURIComponent('http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p'),
+      url: 'http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p',
       name: 'BBC Radio 1'}
-
     ]
 
+  playStation(station: Station) {
+    location.href='http://localhost:3000/play/' + encodeURIComponent(station.url);
+    
+  }
   ngOnInit() {
   }
 
